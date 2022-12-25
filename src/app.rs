73289153531959b25e66fa9fbf1d9e29f2496c30
Ramnath R Iyer <rri@silentyak.com>
2022-12-24 20:09:47 -0800

    Add command-line options

--- src/app.rs
+++ src/app.rs
@@ -5,11 +5,16 @@ use std::io::Result;
 
 /// Arguments to the command-line application.
 #[derive(Parser)]
-#[clap(about)]
-#[clap(version)]
-#[clap(color(ColorChoice::Auto))]
-#[clap(disable_version_flag(true))]
+#[command(about)]
+#[command(version)]
+#[command(arg_required_else_help(true))]
+#[command(color(ColorChoice::Always))]
+#[command(disable_version_flag(true))]
+#[command(help_template("{name} v{version}\n{about}\n\n{usage-heading} {usage}\n\n{all-args}"))]
 struct Args {
+    /// (Optional) Paths to file(s) to open (or create upon save).
+    file: Vec<String>,
+
     /// Print version information and exit.
     #[arg(short, long)]
     version: bool,
@@ -20,7 +25,7 @@ pub fn run() -> Result<()> {
     let args = Args::parse();
 
     if args.version {
-        println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
         println!("{}", env!("CARGO_PKG_DESCRIPTION"));
         return Ok(());
     }
